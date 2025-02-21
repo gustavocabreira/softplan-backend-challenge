4e0f313dc4e3aa2ea23b3c5071771df5ba863042
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-21 17:08:22 -0300

    test: it should return unprocessable entity when payload is invalid

--- tests/Feature/Api/Cakes/CreateCakeTest.php
+++ tests/Feature/Api/Cakes/CreateCakeTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\Cake;
 use Illuminate\Http\Response;
+use Illuminate\Support\Str;
 
 test('it should be able to create a cake', function () {
     $model = new Cake;
@@ -20,3 +21,53 @@ test('it should be able to create a cake', function () {
 
     $this->assertDatabaseCount($model->getTable(), 1);
 });
+
+dataset('invalid_payload', [
+    'empty name' => [
+        ['name' => ''], ['name' => 'The name field is required.'],
+    ],
+    'name with more than 255 characters' => [
+        ['name' => Str::repeat('*', 256)], ['name' => 'The name field must not be greater than 255 characters.'],
+    ],
+    'empty weight' => [
+        ['weight' => ''], ['weight' => 'The weight field is required.'],
+    ],
+    'empty price' => [
+        ['price' => ''], ['price' => 'The price field is required.'],
+    ],
+    'empty quantity' => [
+        ['quantity' => ''], ['quantity' => 'The quantity field is required.'],
+    ],
+    'invalid weight' => [
+        ['weight' => -1], ['weight' => 'The weight must be greater than or equal to 1.'],
+    ],
+    'invalid price' => [
+        ['price' => -1], ['price' => 'The price must be greater than or equal to 0.'],
+    ],
+    'invalid quantity' => [
+        ['quantity' => -1], ['quantity' => 'The quantity must be greater than or equal to 0.'],
+    ],
+    'invalid weight type' => [
+        ['weight' => 'a'], ['weight' => 'The weight must be an integer.'],
+    ],
+    'invalid price type' => [
+        ['price' => 'a'], ['price' => 'The price must be a numeric.'],
+    ],
+    'invalid quantity type' => [
+        ['quantity' => 'a'], ['quantity' => 'The quantity must be an integer.'],
+    ],
+]);
+
+test('it should return unprocessable entity when payload is invalid', function ($payload, $expectedErrors) {
+    $key = array_keys($expectedErrors);
+    $model = new Cake;
+
+    $response = $this->postJson(route('api.cakes.store'), $payload);
+
+    $response
+        ->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+        ->assertJsonValidationErrors($key);
+
+    $this->assertDatabaseMissing($model->getTable(), $payload);
+    $this->assertDatabaseCount($model->getTable(), 0);
+})->with('invalid_payload');
