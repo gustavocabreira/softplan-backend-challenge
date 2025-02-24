3d1397f9993969d76c2417b77d1b61dcce23c92c
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-23 22:36:29 -0300

    test: it should be able to insert emails into subscribers table

--- app/Jobs/HandleFileUploadJob.php
+++ app/Jobs/HandleFileUploadJob.php
@@ -2,45 +2,28 @@
 
 namespace App\Jobs;
 
-use App\Mail\SubscribersImportedSuccessfullyMail;
-use App\Models\EmailList;
+use App\Actions\HandleFileUploadAction;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
-use Illuminate\Support\Facades\Bus;
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Support\Facades\Storage;
 
 class HandleFileUploadJob implements ShouldQueue
 {
     use Queueable;
 
     public function __construct(
-        private int $cakeId,
-        private string $cakeName,
-        private int $listId,
-        private string $filePath
+        public int $cakeId,
+        public string $cakeName,
+        public int $listId,
+        public string $filePath
     ) {}
 
     public function handle(): void
     {
-        $file = Storage::get($this->filePath);
-        $lines = explode("\n", $file);
-        array_shift($lines);
-        array_pop($lines);
-
-        $emails = collect($lines)->map(fn ($email) => trim($email));
-
-        $chunks = $emails->chunk(10000)->map(fn ($chunk) => new ProcessEmailJob($this->cakeId, $chunk->toArray()));
-
-        $cakeName = $this->cakeName;
-        $listId = $this->listId;
-
-        Bus::batch($chunks)
-            ->name('import-emails')
-            ->then(function () use ($cakeName, $listId) {
-                Mail::to('user@example.com')->send(new SubscribersImportedSuccessfullyMail($cakeName));
-                EmailList::query()->find($listId)->update(['status' => 'done']);
-            })
-            ->dispatch();
+        (new HandleFileUploadAction)->execute(
+            cakeId: $this->cakeId,
+            cakeName: $this->cakeName,
+            listId: $this->listId,
+            filePath: $this->filePath
+        );
     }
 }
