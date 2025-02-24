c04555e476d6deab54d9440e0ef904a464109814
Gustavo Cabreira <gustavo.softdev@gmail.com>
2025-02-24 00:18:35 -0300

    it should be able to dispatch a job when file is uploaded

---
+++ tests/Feature/Commands/HandleFileUploadCommandTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Helpers\GenerateCsvData;
+use App\Jobs\HandleFileUploadJob;
+use App\Models\Cake;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Queue;
+
+test('it should be able to dispatch a job when file is uploaded', function () {
+    Queue::fake();
+    $cake = Cake::factory()->create();
+    $file = UploadedFile::fake()->createWithContent('emails.csv', GenerateCsvData::execute(10));
+    $path = $file->storeAs('uploads', uniqid().'.csv');
+    $emailList = $cake->emailLists()->create(['status' => 'pending', 'file_path' => $path]);
+
+    $this->artisan('handle:upload');
+
+    expect($emailList->refresh()->status)->toBe('processing');
+
+    Queue::assertPushed(HandleFileUploadJob::class, 1);
+});
